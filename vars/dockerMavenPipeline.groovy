734f702c1780be8deb6cbc7406b7cca0aafef8c7
bassamelwshahy <bassamelwshahy1@gmail.com>
2025-08-14 21:25:12 +0300

    Update dockerMavenPipeline.groovy ggpp

--- vars/dockerMavenPipeline.groovy
+++ vars/dockerMavenPipeline.groovy
@@ -3,7 +3,7 @@ def call(Map config = [:]) {
     pipelineRunner.runPipeline(
         config.imageName ?: "my-app",
         config.dockerCredsId ?: "dockerhub-creds",
-        config.gitCredsId ?: "github-creds",
+        config.gitCredsId ?: "github-cred",
         config.gitEmail ?: "bassamelwshahy1@gmail.com",
         config.gitRepoUrl ?: "github.com/bassamelwshahy/argocd-nginx-demo.git"
     )
