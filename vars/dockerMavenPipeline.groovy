82a2261ed58775647ce65da80a8cd25b14e86173
bassamelwshahy <bassamelwshahy1@gmail.com>
2025-08-14 20:33:26 +0300

    Update dockerMavenPipeline.groovy ci

--- vars/dockerMavenPipeline.groovy
+++ vars/dockerMavenPipeline.groovy
@@ -1,9 +1,7 @@
 def call(Map config = [:]) {
     def pipelineRunner = new org.example.DockerMavenPipeline(this)
     pipelineRunner.runPipeline(
-        config.imageName ?: "my-app",            // Docker image name
-        config.credentialsId ?: "dockerhub-creds", // DockerHub creds ID
-        config.githubId ?: "github-cred"           // GitHub creds ID
+        config.imageName ?: "default/image",
+        config.credentialsId ?: "dockerhub-creds"
     )
 }
- 
