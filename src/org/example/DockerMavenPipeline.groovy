da2fd8b338c43324511f59af52031110f410c323
bassamelwshahy <bassamelwshahy1@gmail.com>
2025-08-14 21:32:20 +0300

    Update DockerMavenPipeline.groovy cccii

--- src/org/example/DockerMavenPipeline.groovy
+++ src/org/example/DockerMavenPipeline.groovy
@@ -46,12 +46,13 @@ class DockerMavenPipeline implements Serializable {
                     passwordVariable: 'GITHUB_TOKEN'
                 )]) {
                     steps.sh """
+                        git checkout -B main
                         sed -i "s|image: .*|image: bassamelwshahy/my-app:${steps.env.BUILD_NUMBER}|" deployment.yaml
                         git config --global user.email "${gitEmail}"
                         git config --global user.name "Jenkins CI"
                         git add deployment.yaml
                         git commit -m "Update image tag to ${steps.env.BUILD_NUMBER}" || echo "No changes to commit"
-                        git push https://${GITHUB_USER}:${GITHUB_TOKEN}@${gitRepoUrl} HEAD:main
+                        git push https://${steps.env.GITHUB_USER}:${steps.env.GITHUB_TOKEN}@${gitRepoUrl} HEAD:main
                     """
                 }
             }
