6379fbef42faf3941438222e893266c1c1eb4c47
bassamelwshahy <bassamelwshahy1@gmail.com>
2025-08-14 20:41:44 +0300

    Update DockerMavenPipeline.groovy ci

--- src/org/example/DockerMavenPipeline.groovy
+++ src/org/example/DockerMavenPipeline.groovy
@@ -8,7 +8,7 @@ class DockerMavenPipeline implements Serializable {
         this.steps = steps
     }
 
-    def runPipeline(String imageName, String credentialsId, String githubId) {
+    def runPipeline(String imageName, String credentialsId) {
         steps.node {
             steps.env.IMAGE_NAME = imageName
 
@@ -17,34 +17,25 @@ class DockerMavenPipeline implements Serializable {
             }
 
             steps.stage('Maven Build (in Docker)') {
-                steps.sh """
-                    docker run --rm \
-                        -v ${steps.env.WORKSPACE}:/workspace \
-                        -w /workspace \
-                        maven:3.9.5-eclipse-temurin-17 \
-                        mvn -B clean package
-                """
+                steps.sh "docker run --rm -v ${steps.env.WORKSPACE}:/workspace -w /workspace maven:3.9.5-eclipse-temurin-17 mvn -B clean package"
                 steps.archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
             }
 
             steps.stage('Docker Build') {
                 def tag = "${steps.env.BUILD_NUMBER}"
-                steps.sh """
-                    docker build -t bassamelwshahy/${imageName}:${tag} \
-                                 -t bassamelwshahy/${imageName}:latest .
-                """
+                steps.sh "docker build -t ${imageName}:${tag} -t ${imageName}:latest ."
                 steps.sh 'docker image prune -f || true'
             }
 
             steps.stage('Docker Push') {
                 steps.withCredentials([steps.usernamePassword(
-                    credentialsId: credentialsId,
-                    usernameVariable: 'DOCKER_USER',
-                    passwordVariable: 'DOCKER_PASS'
+                        credentialsId: credentialsId,
+                        usernameVariable: 'DOCKER_USER',
+                        passwordVariable: 'DOCKER_PASS'
                 )]) {
                     steps.sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
-                    steps.sh "docker push bassamelwshahy/${imageName}:${steps.env.BUILD_NUMBER}"
-                    steps.sh "docker push bassamelwshahy/${imageName}:latest"
+                    steps.sh "docker push ${imageName}:${steps.env.BUILD_NUMBER}"
+                    steps.sh "docker push ${imageName}:latest"
                 }
             }
         }
